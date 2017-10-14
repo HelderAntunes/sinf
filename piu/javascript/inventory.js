6a05dcf2cbc5bab373c4766d2341624f772dafd1
inesfproenca <inesfproenca@gmail.com>
2017-10-14 19:26:33 +0100

    Inventory page changed

--- piu/javascript/inventory.js
+++ piu/javascript/inventory.js
@@ -1,69 +1,66 @@
 $(document).ready(function () {
 
-    Highcharts.chart('stock-available', {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Stock available'
-        },
-        subtitle: {
-            text: 'by product'
-        },
-        xAxis: {
-            categories: [
-                'Jan',
-                'Feb',
-                'Mar',
-                'Apr',
-                'May',
-                'Jun',
-                'Jul',
-                'Aug',
-                'Sep',
-                'Oct',
-                'Nov',
-                'Dec'
-            ],
-            crosshair: true
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Number of items (M)'
-            }
-        },
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [{
-            name: 'Product A',
-            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+  Highcharts.chart('stock-available', {
+      title: {
+          text: 'Stock history'
+      },
 
-        }, {
-            name: 'Product B',
-            data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+      subtitle: {
+          text: 'By category'
+      },
+      xAxis: {
+          categories: ['9 Oct', '10 Oct', '11 Oct', '12 Oct', '13 Oct', '14 Oct', '15 Oct', '16 Oct']
+      },
+      yAxis: {
+          title: {
+              text: 'Stock'
+          }
+      },
+      legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'middle'
+      },
 
-        }, {
-            name: 'Product C',
-            data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+      plotOptions: {
+          series: {
+              label: {
+                  connectorAllowed: false
+              }
+          }
+      },
 
-        }, {
-            name: 'Product D',
-            data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+      series: [{
+          name: 'Category 1',
+          data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+      }, {
+          name: 'Category 2',
+          data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+      }, {
+          name: 'Category 3',
+          data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+      }, {
+          name: 'Category 4',
+          data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+      }, {
+          name: 'Category 5',
+          data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+      }],
 
-        }]
+      responsive: {
+          rules: [{
+              condition: {
+                  maxWidth: 500
+              },
+              chartOptions: {
+                  legend: {
+                      layout: 'horizontal',
+                      align: 'center',
+                      verticalAlign: 'bottom'
+                  }
+              }
+          }]
+      }
     });
 
     Highcharts.chart('inventory-value', {
@@ -85,7 +82,7 @@ $(document).ready(function () {
             pie: {
                 dataLabels: {
                     enabled: true,
-                    distance: -50,
+                    distance: -30,
                     style: {
                         fontWeight: 'bold',
                         color: 'white'
@@ -109,96 +106,126 @@ $(document).ready(function () {
         }]
     });
 
-    Highcharts.chart('inventory-days', {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Average Inventory Days'
-        },
-        subtitle: {
-            text: 'by Product'
-        },
-        xAxis: {
-            type: 'category'
-        },
-        yAxis: {
-            title: {
-                text: 'Days'
-            }
+    Highcharts.chart('best-seller-inventory', {
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: 'Best Seller Stock'
+      },
+      subtitle: {
+          text: 'Click the columns to view stock history over the last days'
+      },
+      xAxis: {
+          type: 'category'
+      },
+      yAxis: {
+          title: {
+              text: 'Unit in stock'
+          }
 
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            series: {
-                borderWidth: 0,
-                dataLabels: {
-                    enabled: true,
-                    format: '{point.y:.1f}'
-                }
-            }
-        },
+      },
+      legend: {
+          enabled: false
+      },
+      plotOptions: {
+          series: {
+              borderWidth: 0,
+              dataLabels: {
+                  enabled: true,
+                  format: '{point.y}'
+              }
+          }
+      },
 
-        tooltip: {
-            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-        },
+      tooltip: {
+          headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+          pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> of total<br/>'
+      },
 
-        series: [{
-            name: 'Pruducts',
-            colorByPoint: true,
-            data: [{
-                name: 'Product A',
-                y: 56.33,
-                drilldown: 'Product_A'
-            }, {
-                name: 'Product B',
-                y: 24.03,
-                drilldown: 'Product_A'
-            }, {
-                name: 'Product C',
-                y: 10.38,
-                drilldown: 'Product_A'
-            }, {
-                name: 'Product D',
-                y: 4.77,
-                drilldown: 'Product_A'
-            }]
-        }],
-        drilldown: {
-            series: [{
-                name: 'Product A',
-                id: 'Product_A',
-                data: [
-                    [
-                        'Jun',
-                        24.13
-                    ],
-                    [
-                        'Jul',
-                        17.2
-                    ],
-                    [
-                        'Aug',
-                        8.11
-                    ],
-                    [
-                        'Sep',
-                        5.33
-                    ],
-                    [
-                        'Oct',
-                        1.06
-                    ],
-                    [
-                        'Nov',
-                        0.5
-                    ]
-                ]
-            }]
-        }
+      series: [{
+          name: 'Best Sellers',
+          colorByPoint: true,
+          data: [{
+              name: 'Product 7',
+              y: 568,
+              drilldown: 'Product 7'
+          }, {
+              name: 'Product 1',
+              y: 25,
+              drilldown: 'Product 1'
+          }, {
+              name: 'Product 6',
+              y: 120,
+              drilldown: 'Product 6'
+          }]
+      }],
+      drilldown: {
+          series: [{
+              name: 'Product 7',
+              id: 'Product 7',
+              data: [
+                  [
+                      '13 Oct',
+                      368
+                  ],
+                  [
+                      '14 Oct',
+                      24
+                  ],
+                  [
+                      '15 Oct',
+                      700
+                  ],
+                  [
+                      '16 Oct',
+                      568
+                  ]
+              ]
+          }, {
+              name: 'Product 1',
+              id: 'Product 1',
+              data: [
+                  [
+                      '13 Oct',
+                      276
+                  ],
+                  [
+                      '14 Oct',
+                      132
+                  ],
+                  [
+                      '15 Oct',
+                      56
+                  ],
+                  [
+                      '16 Oct',
+                      25
+                  ]
+              ]
+          }, {
+              name: 'Product 6',
+              id: 'Product 6',
+              data: [
+                  [
+                      '13 Oct',
+                      324
+                  ],
+                  [
+                      '14 Oct',
+                      77
+                  ],
+                  [
+                      '15 Oct',
+                      240
+                  ],
+                  [
+                      '16 Oct',
+                      120
+                  ]
+              ]
+          }]
+      }
     });
 
 });
