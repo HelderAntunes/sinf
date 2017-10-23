50978a1ecb154a5f90a41af743264e66b12629d5
unknown <joaofilipepereiracosta@hotmail.com>
2017-10-23 12:55:34 +0100

    Create Primavera Integration Project; purchases list done;

---
+++ proto/PrimaveraIntegration/Controllers/PurchasesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PrimaveraIntegration.Lib_Primavera.Model;
+
+namespace PrimaveraIntegration.Controllers
+{
+    public class PurchasesController : ApiController
+    {
+        // GET api/purchases   
+        public IEnumerable<Lib_Primavera.Model.Purchase> GetPurchases()
+        {
+            return Lib_Primavera.PriIntegration.GetAllPurchases(null, null);
+        }
+
+        // GET api/purchases/date/{year}
+        public IEnumerable<Lib_Primavera.Model.Purchase> GetPurchases(int year)
+        {
+            return Lib_Primavera.PriIntegration.GetAllPurchases(year.ToString(), null);
+        }
+
+        public IEnumerable<Lib_Primavera.Model.Purchase> GetPurchases(int year, int month)
+        {
+            return Lib_Primavera.PriIntegration.GetAllPurchases(year.ToString(), month.ToString());
+        }
+
+        /*public Lib_Primavera.Model.Purchase Get(int id)
+        {
+            var product = Lib_Primavera.PriIntegration.GetAllPurchases(null, null).FirstOrDefault((p) => p.Id == id.ToString());
+            if (product == null)
+            {
+                throw new HttpResponseException(
+                  Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return product;
+        }*/
+    }
+}
