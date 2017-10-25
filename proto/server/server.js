2036bb93e96fedaa26e7e1d960878848777bbb5b
helder <helder_antunes-@hotmail.com>
2017-10-25 10:28:09 +0100

    Templating sales page.

--- proto/server/server.js
+++ proto/server/server.js
@@ -29,13 +29,31 @@ app.get('/main', function (req, res) {
 
 app.get('/sales', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
+
+    var headerTpl = fs.readFileSync('./templates/common/header.html', {encoding: 'utf-8'});
+    var compiledTemplate = new jSmart(headerTpl);
+    var outputHeader = compiledTemplate.fetch({ title: 'Sales Dashboard'});
+
+    var salesTpl = fs.readFileSync('./templates/sales.html', {encoding: 'utf-8'});
+    compiledTemplate = new jSmart(salesTpl);
+    var outputSales = compiledTemplate.fetch({
+        totalSales: formatNumber(154175),
+        period: 'year',
+        salesPerPeriod: formatNumber(25),
+    });
     
-    var tpl = fs.readFileSync('./templates/sales.html', {encoding: 'utf-8'});
-    var compiledTemplate = new jSmart(tpl);
-    var output = compiledTemplate.fetch();
+    var footerTpl = fs.readFileSync('./templates/common/footer.html', {encoding: 'utf-8'});
+    compiledTemplate = new jSmart(footerTpl);
+    var outputFooter = compiledTemplate.fetch();
     
-    res.end(output);
+    res.end(outputHeader + outputSales + outputFooter);
 });
+
+function formatNumber(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    return parts.join(".");
+}
 
 app.get('/finances', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
