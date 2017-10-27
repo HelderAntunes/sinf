9839535755d3e023813a3b6eb5b6a8dedd3a41bd
iddani <danielaqfsa@gmail.com>
2017-10-27 17:38:09 +0100

    getPurchaseOrders and getTotalClientDebt from SAF-T

--- proto/server/saft_parser.js
+++ proto/server/saft_parser.js
@@ -1,10 +1,12 @@
-var fs = require('fs'); 
+var fs = require('fs');
 var parseString = require('xml2js').parseString;
 
 fs.readFile('../assets/SAFT_DEMOSINF_01-01-2016_31-12-2016.xml', function(err, data) {
     parseString(data, function (err, result) {
-        result = getSalesInvoices(result);        
-        // result = getCostumers(result);
+        //result = getSalesInvoices(result);
+        //result = getCostumers(result);
+        //result = getPurchaseOrders(result);
+        result = getTotalClientDebt(result); //returns int
         fs.writeFile('saft_in_json.js', JSON.stringify(result, null, 2), function (err) {
             if (err) throw err;
             console.log('SAF-T xml parsed.');
@@ -18,4 +20,34 @@ function getCostumers(saftParsed) {
 
 function getSalesInvoices(saftParsed) {
     return saftParsed['AuditFile']['SourceDocuments'][0]['SalesInvoices'][0]['Invoice'];
+}
+
+//encomendas
+function getPurchaseOrders(saftParsed) {
+    //ir buscar work docs
+    workingDoc = saftParsed['AuditFile']['SourceDocuments'][0]['WorkingDocuments'][0]['WorkDocument'];
+    docType = workingDoc[0]['WorkType'][0];
+
+    //verificar que e encomenda e adicionar a array ret
+    var ret = new Array();
+    if(docType == "NE"){
+        ret.push(workingDoc);
+        //console.log("ne: " + JSON.stringify(workingDoc, null, 2));
+    }
+
+    return ret;
+}
+
+//divida total
+function getTotalClientDebt(SaftParsed){
+    //preencher array com todas as invoices
+    invoices = getSalesInvoices(SaftParsed);
+
+    //preencher ret com dividaTotal
+    ret = 0;
+    for (var i = 0, len = invoices.length; i < len; i++) {
+        ret +=  parseInt(invoices[i]["Line"][0]["CreditAmount"][0]);
+    }
+
+    return ret;
 }
