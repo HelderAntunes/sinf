e860f7a81a07555fe176943ee10c6ad129e22009
inesfproenca <inesfproenca@gmail.com>
2017-11-10 10:27:50 +0000

    Month selection working (visually not selected)

--- proto/server/public/javascript/purchases.js
+++ proto/server/public/javascript/purchases.js
@@ -55,7 +55,7 @@ var updateSuppliers = function ($scope, $http) {
     var url = 'http://localhost:49822/api/Purchases/groupBySupplier?year=' + $scope.chosenYear;
 
     if($scope.chosenMonth != null){
-        url += '&month=' + $scope.chooseMonth;
+        url += '&month=' + $scope.chosenMonth;
     }
 
     $http.get(url).then(function (success){
@@ -82,7 +82,7 @@ var updateGraph = function($scope, $http){
     var url = 'http://localhost:49822/api/Purchases/groupByDate?year=' + $scope.chosenYear;
 
     if($scope.chosenMonth != null){
-        url += '&month=' + $scope.chooseMonth;
+        url += '&month=' + $scope.chosenMonth;
     }
 
     $http.get(url).then(function (success){
@@ -111,7 +111,7 @@ var updateGrowth = function($scope, $http){
     var url = 'http://localhost:49822/api/Purchases/groupByDate?year=' + ($scope.chosenYear - 1);
 
     if($scope.chosenMonth != null){
-        url += '&month=' + $scope.chooseMonth;
+        url += '&month=' + $scope.chosenMonth;
     }
 
     $http.get(url).then(function (success){
@@ -154,7 +154,7 @@ app.controller('purchases_controller', function($scope, $http) {
     $scope.chosenYear = 2017;
     $scope.chosenMonth = null;
     $scope.years = [2015,2016,2017,2018];
-    $scope.months = [{value: 1, name: 'Jan'},{value: 2, name: 'Feb'},{value: 3, name: 'Mar'},{value: 4, name: 'Apr'},{value: 5, name: 'May'},{value: 6, name: 'Jun'},
+    $scope.months = [{value: null, name: 'None'},{value: 1, name: 'Jan'},{value: 2, name: 'Feb'},{value: 3, name: 'Mar'},{value: 4, name: 'Apr'},{value: 5, name: 'May'},{value: 6, name: 'Jun'},
     {value: 7, name: 'Jul'},{value: 8, name: 'Aug'},{value: 9, name: 'Sep'},{value: 10, name: 'Oct'},{value: 11, name: 'Nov'},{value: 12, name: 'Dec'}];
 
     $scope.chooseYear = function(year){
@@ -162,8 +162,10 @@ app.controller('purchases_controller', function($scope, $http) {
         updateData($scope, $http);
     };
 
-    $scope.debug = function(){
+    $scope.chooseMonth = function(month){
+        $scope.chosenMonth = month;
         console.log($scope.chosenYear + '/' + $scope.chosenMonth);
+        updateData($scope, $http);
     };
 
     updateData($scope,$http);
