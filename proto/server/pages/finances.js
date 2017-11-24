8257a584a371315148ea910921133d69ed3a9c3e
Hélder Antunes <helder_antunes-@hotmail.com>
2017-11-24 11:11:26 +0000

    Finances begin

---
+++ proto/server/pages/finances.js
@@ -0,0 +1,23 @@
+var fs = require('fs'); 
+var jSmart = require('jsmart'); 
+var utils = require('../utils');
+
+exports.getFinances = function (req, res) {
+    
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    var headerTpl = fs.readFileSync('./templates/common/header.html', {encoding: 'utf-8'});
+    var compiledTemplate = new jSmart(headerTpl);
+    var outputHeader = compiledTemplate.fetch({ title: 'Finances Dashboard'});
+
+    var financesTpl = fs.readFileSync('./templates/finances.html', {encoding: 'utf-8'});
+    compiledTemplate = new jSmart(financesTpl);
+    var outputFinances = compiledTemplate.fetch();
+    
+    var footerTpl = fs.readFileSync('./templates/common/footer.html', {encoding: 'utf-8'});
+    compiledTemplate = new jSmart(footerTpl);
+    var outputFooter = compiledTemplate.fetch();
+    
+    res.end(outputHeader + outputFinances + outputFooter);
+
+}
